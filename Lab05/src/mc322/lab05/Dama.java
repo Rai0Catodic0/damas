8efc686d55ad7c36d23a7c302724667af2b5997f
Jéssica Oliveira <jessica.oliveira@conpec.com.br>
2021-04-23 13:15:49 -0300

    acrescentei funcionalidades básicas em dama e peao para testar o tabuleiro

--- Lab05/src/mc322/lab05/Dama.java
+++ Lab05/src/mc322/lab05/Dama.java
@@ -1,5 +1,17 @@
 package mc322.lab05;
 
 public class Dama {
-	
+	int i,j;
+	char tipo;
+	boolean preto;
+	Dama(int i, int j, boolean preto){
+		this.i = i;
+		this.j = j;
+		this.preto = preto;
+		if(preto) {
+			this.tipo = 'P';
+		} else {
+			this.tipo = 'B';
+		}
+	}
 }
--- Lab05/src/mc322/lab05/Peão.java
+++ Lab05/src/mc322/lab05/Peão.java
@@ -1,5 +1,17 @@
 package mc322.lab05;
 
 public class Peão {
-
+	int i,j;
+	char tipo;
+	boolean preto;
+	Peão(int i, int j, boolean preto){
+		this.i = i;
+		this.j = j;
+		this.preto = preto;
+		if(preto) {
+			this.tipo = 'p';
+		} else {
+			this.tipo = 'b';
+		}
+	}
 }
