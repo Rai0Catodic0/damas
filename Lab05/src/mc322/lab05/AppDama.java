27e2c0588ba64965fe23a845c69348de880be5ec
Jéssica Oliveira <jessica.oliveira@conpec.com.br>
2021-04-23 12:59:09 -0300

    inicio da main, tabuleiro e csv

--- Lab05/src/mc322/lab05/AppDama.java
+++ Lab05/src/mc322/lab05/AppDama.java
@@ -1,5 +1,25 @@
 package mc322.lab05;
 
 public class AppDama {
-
+	public static void main(String args[]) {
+		//CSVReader csv = new CSVReader();
+	    //csv.setDataSource("");
+	    //String commands[] = csv.requestCommands();
+		String commands[] = {"4c:5e","3d:4e"};
+		executaJogo(commands);
+	}
+	
+	public static void executaJogo(String commands[]) {
+		Tabuleiro dama = new Tabuleiro();
+		int n = commands.length;
+		System.out.printf("Tabuleiro Inicial\n");
+		dama.Apresentar();
+		for(int i = 0; i<n; i++) {
+			dama.Mover(commands[i]);
+			System.out.println();
+			System.out.print("Source: "+commands[i].charAt(0)+commands[i].charAt(1)+"\n");
+			System.out.print("Target: "+commands[i].charAt(3)+commands[i].charAt(4)+"\n");
+		}
+	}
+	
 }
--- Lab05/src/mc322/lab05/Tabuleiro.java
+++ Lab05/src/mc322/lab05/Tabuleiro.java
@@ -1,5 +1,50 @@
 package mc322.lab05;
 
 public class Tabuleiro {
+	Peão peoes[][] = new Peão[7][7];
+	Dama damas[][] = new Dama[7][7];
+	int numeroDePinos = 32;
+	Tabuleiro() {
+		for(int i=0;i<8;i++) {
+			for(int j = 0; j<8; j++) {
+				if(i>2&&i<4) {
+					peoes[i][j] = null;
+				} else if(i==5 || i==7) {
+					if(j%2==0) {
+						peoes[i][j]=new Peão(i,j,true);
+					}
+				} else if (i==6 && j%2 != 0) {
+					peoes[i][j]=new Peão(i,j,true);
+				} else if(i==0||i==2) {
+					if(j%2!=0) {
+						peoes[i][j]=new Peão(i,j,false);
+					}
+				}else if(i==1 && j%2==0) {
+					peoes[i][j] = new Peão(i,j,false);
+				}
+				damas[i][j] = null;
+			}
+		}
+
+	}
+
+	String Apresentar() {
+		for(int i=0;i<8;i++) {
+			for(int j = 0; j<8; j++) {
+				//System.out.print(); -> printar atributo do peão
+				//fazer lógica para ver se é dama ou peão e printar espaço só se 
+				//os dois estão vazios
+			}
+		}
+	}
+
+	void Mover(String comando) {
+		//transformar coordenadas
+		int jOrigem = comando.charAt(0)-97;
+		int iOrigem = 55-comando.charAt(1);
+		int jDestino = comando.charAt(3)-97;
+		int iDestino = 55-comando.charAt(4);
+		
+	}
 
 }
